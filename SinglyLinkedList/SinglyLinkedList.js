805c37720ae9ab462835d1ff79af52288eb1af52
8BeatTakeshi <d.nakamura75@gmail.com>
2021-11-11 17:36:20 +0100

    (SinglyLinkedList): implement push method to insert a new node

--- SinglyLinkedList/SinglyLinkedList.js
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -11,4 +11,30 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    if (!val) {
+      throw new Error('The push method expects a value in argument');
+    }
+    // console.log(this.head, this.tail, this.length);
+
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    // console.log(this.head, this.tail, this.length);
+
+    return this;
+  }
 }
+
+let list = new SinglyLinkedList();
+list.push('Hello');
+list.push('Goodbye');
+console.log(list.head.next);
