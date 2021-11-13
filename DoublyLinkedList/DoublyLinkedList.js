04dee15cb6c7a4b3b050e533ce3bd90d156ba3ab
8BeatTakeshi <d.nakamura75@gmail.com>
2021-11-13 16:10:38 +0100

    (DoublyLinkedList): implement push method to insert a node at the end of the list

--- DoublyLinkedList/DoublyLinkedList.js
+++ DoublyLinkedList/DoublyLinkedList.js
@@ -12,4 +12,35 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(data) {
+    if (data === undefined) {
+      return;
+    }
+
+    let newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push('1');
+list.push('2');
+list.push('3');
+list.push('4');
+list.push('5');
+
+console.log(list);
+console.log(list.head);
+console.log(list.tail);
