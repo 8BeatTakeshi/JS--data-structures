8fd6d5df34e685e7a07acafa9d4688b1b9a37ec5
8BeatTakeshi <d.nakamura75@gmail.com>
2021-11-14 17:11:55 +0100

    (Stack): implement a stack Class and push method to add a node in the stack

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,6 +1,38 @@
 class Node {
   constructor(data) {
     this.data = data;
-    this.next = null;
+    this.prev = null;
   }
 }
+
+class Stack {
+  constructor() {
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(data) {
+    if (data === undefined) {
+      return;
+    }
+
+    let newNode = new Node(data);
+    if (!this.last) {
+      this.last = newNode;
+    } else {
+      newNode.prev = this.last;
+      this.last = newNode;
+    }
+
+    // this.size++;
+    return ++this.size;
+  }
+}
+
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+console.log(stack);
