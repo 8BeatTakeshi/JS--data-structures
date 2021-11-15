c2142c25f80dd0624a541d764aad96980305276b
8BeatTakeshi <d.nakamura75@gmail.com>
2021-11-15 19:54:30 +0100

    (BinarySearchTree): implement a find  method to insert a node in the tree, and refactor insert and find method with recursive way

--- BinarySearchTree/BinarySearchTree.js
+++ BinarySearchTree/BinarySearchTree.js
@@ -11,55 +11,93 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(data) {
-    if (data === undefined) {
-      return undefined;
-    }
+  ////////// RECURSIVE INSERT AND FIND METHOD //////////
+
+  insert(data, current = this.root) {
+    if (data === undefined) return undefined;
 
     let newNode = new Node(data);
-    if (!this.root) {
+
+    if (!current) {
       this.root = newNode;
       return this;
     }
-    // let current = this.root;
-    // while (true) {
-    //   if (data === current.data) {
-    //     return undefined;
-    //   }
-    //   if (data < current.data) {
-    //     if (current.left === null) {
-    //       current.left = newNode;
-    //       return this;
-    //     }
-    //     current = current.left;
-    //   } else if (data > current.data) {
-    //     if (current.right === null) {
-    //       current.right = newNode;
-    //       return this;
-    //     }
-    //     current = current.right;
-    //   }
-    // }
+    if (current.data === data) return false;
 
-    // a more understandable way
-    let current;
-    let next = this.root;
-    let direction;
-
-    do {
-      current = next;
-      if (current.data === data) {
-        return undefined;
-      }
-
-      direction = current.data > data ? 'left' : 'right';
-      next = current[direction];
-    } while (next);
-
-    current[direction] = newNode;
-
-    return this;
+    const direction = current.data > data ? 'left' : 'right';
+    if (!current[direction]) {
+      current[direction] = newNode;
+      return this;
+    } else {
+      return this.insert(data, current[direction]);
+    }
   }
+
+  find(data, current = this.root) {
+    if (data === undefined) return undefined;
+    if (!current) return false;
+
+    if (data === current.data) return current; // or true
+    return current.data > data
+      ? this.find(data, current.left)
+      : this.find(data, current.right);
+  }
+
+  ////////// ITERATIVE INSERT AND FIND METHOD //////////
+
+  // insert(data) {
+  //   if (data === undefined) {
+  //     return undefined;
+  //   }
+
+  //   let newNode = new Node(data);
+  //   if (!this.root) {
+  //     this.root = newNode;
+  //     return this;
+  //   }
+
+  //   let current;
+  //   let next = this.root;
+  //   let direction;
+
+  //   do {
+  //     current = next;
+  //     if (current.data === data) {
+  //       return undefined;
+  //     }
+
+  //     direction = current.data > data ? 'left' : 'right';
+  //     next = current[direction];
+  //   } while (next);
+
+  //   current[direction] = newNode;
+
+  //   return this;
+  // }
+
+  // find(data) {
+  //   if (data === undefined) {
+  //     return undefined;
+  //   }
+  //   if (!this.root) {
+  //     return false;
+  //   }
+
+  //   let current;
+  //   let next = this.root;
+  //   let direction;
+
+  //   do {
+  //     current = next;
+  //     if (current.data === data) {
+  //       return current; // or return true
+  //     }
+  //     direction = current.data > data ? 'left' : 'right';
+  //     next = current[direction];
+  //   } while (next);
+
+  //   return false;
+  // }
 }
 
 let tree = new BinarySearchTree();
@@ -73,3 +111,4 @@ tree.insert(79);
 
 console.log(tree);
 console.log(tree.insert(79));
+console.log(tree.find(37));
